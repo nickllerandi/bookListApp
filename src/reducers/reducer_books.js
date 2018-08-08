60b8b46dfeb290fd66df6f75e6931994abf1b410
nicholas llerandi <nicholasllerandi@gmail.com>
2018-08-07 20:31:03 -0400

    initial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: "harry potter" },
+        { title: "sapiens" },
+        { title: "the water fountain" },
+        { title: "witches brew" }
+    ]
+}
